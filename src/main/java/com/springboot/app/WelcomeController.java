9800f6a8c9482ccbf0b12aed7ed4a23c5d55ad6c
prasad-shanbhag <103826854+prasad-shanbhag@users.noreply.github.com>
2023-07-31 18:40:59 +0530

    Update WelcomeController.java

--- src/main/java/com/springboot/app/WelcomeController.java
+++ src/main/java/com/springboot/app/WelcomeController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
  @GetMapping("/")
 	public String welcome() {
-	 return "welcome to spring boot app made by SWAMIPRASAD SHANBHAG";
+	 return "welcome to spring boot app made by SWAMIPRASAD";
  }
 }
